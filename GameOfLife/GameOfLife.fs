1198017cdf78f7ced264fac6e21edb10a8ef9342
Ian Russell <i.j.russell@tesco.net>
2017-11-11 13:15:50 +0000

    is_alive implemented

---
+++ GameOfLife/GameOfLife.fs
@@ -0,0 +1,11 @@
+﻿module GameOfLifeCode
+
+    let isAlive neighbours status =
+        match neighbours with
+        | 3 -> 1
+        | 2 -> status
+        | _ -> 0
+    
+    // neighbour count
+    // tick event board -> board
+
---
+++ GameOfLife/GameOfLifeTests.fs
@@ -0,0 +1,23 @@
+﻿module GameOfLifeTests
+
+open NUnit.Framework
+open FsUnit
+open GameOfLifeCode
+
+[<Test>]
+let ``Less than 2 or greater than 3 neighbours return dead`` 
+    ([<Values(0,1,4,5)>] n, [<Values(0,1)>] status) =
+        isAlive n status
+        |> should equal 0
+
+[<Test>]
+let ``Exactly 3 neighbours return alive``
+    ([<Values(0,1)>] status) =
+        isAlive 3 status
+        |> should equal 1
+    
+[<Test>]
+let ``2 neighbours return current status`` 
+    ([<Values(0,1)>] status) =
+        isAlive 2 status
+        |> should equal status
