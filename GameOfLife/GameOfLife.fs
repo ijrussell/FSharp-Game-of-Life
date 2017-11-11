e9328632930dc3459965d3f5ad116d1fa2fa2892
Ian Russell <i.j.russell@tesco.net>
2017-11-11 13:45:53 +0000

    neighbour_count implemented

--- GameOfLife/GameOfLife.fs
+++ GameOfLife/GameOfLife.fs
@@ -6,6 +6,19 @@
         | 2 -> status
         | _ -> 0
     
-    // neighbour count
+    let notSourceCell x y x' y' =
+        not (x = x' && y = y')
+
+    let inBounds x y size =
+        x >= 0 && x < size && y >= 0 && y < size
+    
+    let neighbourCount x y (board:int[,]) =
+        [ for dx in -1 .. +1 do
+            for dy in -1 .. +1 do
+                let m, n = x + dx, y + dy
+                if (notSourceCell x y m n) && (inBounds m n (board.GetLength 0) ) then
+                    yield board.[m, n] ]
+        |> List.sum
+
     // tick event board -> board
 
--- GameOfLife/GameOfLifeTests.fs
+++ GameOfLife/GameOfLifeTests.fs
@@ -21,3 +21,43 @@ let ``2 neighbours return current status``
     ([<Values(0,1)>] status) =
         isAlive 2 status
         |> should equal status
+
+let emptyBoard = Array2D.init 3 3 (fun _ _ -> 0)
+
+[<Test>]
+let ``No neighbours returns 0`` () =
+    let board = Array2D.copy emptyBoard
+    neighbourCount 1 1 board
+    |> should equal 0
+
+[<Test>]
+let ``All alive neighbours returns 8`` () =
+    neighbourCount 1 1 (Array2D.init 3 3 (fun _ _ -> 1))
+    |> should equal 8
+
+[<Test>]
+let ``Corner returns 3 alive neighbours`` 
+    ([<Values(0, 2)>] x, [<Values(0, 2)>] y) =
+        neighbourCount x y (Array2D.init 3 3 (fun _ _ -> 1))
+        |> should equal 3
+
+[<Test>]
+let ``Vertical/horizontal cross returns 4`` () =
+    let board = Array2D.copy emptyBoard
+    board.[1,0] <- 1
+    board.[0,1] <- 1
+    board.[1,2] <- 1
+    board.[2,1] <- 1
+    neighbourCount 1 1 board
+    |> should equal 4
+
+[<Test>]
+let ``Diagonal cross returns 4`` () =
+    let board = Array2D.copy emptyBoard
+    board.[0,0] <- 1
+    board.[0,2] <- 1
+    board.[2,0] <- 1
+    board.[2,2] <- 1
+    neighbourCount 1 1 board
+    |> should equal 4
+
