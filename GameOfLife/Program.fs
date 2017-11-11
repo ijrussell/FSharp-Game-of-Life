1740196b30ac50e9d52464605bace2f9d47111ff
Ian Russell <i.j.russell@tesco.net>
2017-11-11 14:05:44 +0000

    completed program

--- GameOfLife/Program.fs
+++ GameOfLife/Program.fs
@@ -1,9 +1,23 @@
-﻿// print a cell
-// print board
-// recusion for game
-// intialize the board
+﻿let printCell x y state =
+    if (state <> 0) then
+        System.Console.SetCursorPosition(x, y)
+        System.Console.Write("#")
+
+let printBoard board =
+    System.Console.Clear()
+    board
+    |> Array2D.iteri (fun x y v -> printCell x y v)
+
+let rec game board =
+    printBoard board
+    System.Threading.Thread.Sleep(100)
+    GameOfLifeCode.tick board 
+    |> game
+
+let rnd = System.Random()
+let initialBoard size = Array2D.init size size (fun _ _ -> rnd.Next(2))
 
 [<EntryPoint>]
 let main argv = 
-    // start game
+    game (initialBoard 30)
     0 // return an integer exit code
